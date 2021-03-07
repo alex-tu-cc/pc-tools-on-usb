d1e07997fa155e8c56d155f2475da3ca506e50e9
Alex Tu <alex.tu@canonical.com>
2021-03-07 23:15:58 +0800

    skip storage selecting

--- pc-sanity-scripts/disable-uattu.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import softwareproperties
-from softwareproperties import SoftwareProperties
-s = SoftwareProperties.SoftwareProperties()
-
-s.set_update_automation_level(softwareproperties.UPDATE_MANUAL)
-
-print("OK")
